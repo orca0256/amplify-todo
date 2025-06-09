223c71a34b591ec6ff521bfd307743ba83bf48a7
Hiroto Kasami <44905466+orca0256@users.noreply.github.com>
2025-06-09 22:09:14 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,9 @@ const client = generateClient<Schema>();
 
 function App() {
 
-    const { signOut } = useAuthenticator();
+    const { user, signOut } = useAuthenticator();
+
+
   
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   
@@ -33,7 +35,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+     <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
