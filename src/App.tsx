c491160ce0c456982459bd5dbfae04fbb3f194f4
Hiroto Kasami <44905466+orca0256@users.noreply.github.com>
2025-06-10 14:35:36 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,68 +5,86 @@ import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
   
- const { errors, data: newTodo } = await client.models.Todo.create(
-  {
-    content: 'My new todo',
-  },
-  {
-    authMode: 'userPool',
-  }
-); 
 
 
 function App() {
+  const { user, signOut } = useAuthenticator();
+  const [todos, setTodos] = useState<Schema["Todo"]["type"][]>([]);
 
-    const { user, signOut } = useAuthenticator();
-
-
-  
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  
-  
-
-  
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+    let subscription;
+    if (user) {
+      // ユーザーがログインしているときのみ subscribe
+      subscription = client.models.Todo
+        .observeQuery({ authMode: "userPool" })
+        .subscribe({
+          next: ({ items }) => setTodos(items),
+          error: (err) => console.error(err),
+        });
+    } else {
+      // ログアウト時にはリストをクリア
+      setTodos([]);
+    }
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-    function deleteTodo(id: string) {
-    client.models.Todo.delete({ id })
+    return () => subscription?.unsubscribe();
+  }, [user]);
+
+  // create 時にも authMode を指定
+  async function createTodo() {
+    const content = window.prompt("Todo content");
+    if (!content || !user) return;
+
+    const { data, errors } = await client.models.Todo.create(
+      { content },
+      { authMode: "userPool" }
+    );
+
+    if (errors) {
+      console.error("Create error:", errors);
+    } else {
+      console.log("Created:", data);
+    }
   }
 
+  // delete 時にも authMode を指定
+  async function deleteTodo(id: string) {
+    if (!user) return;
+
+    const { errors } = await client.models.Todo.delete(
+      { id },
+      { authMode: "userPool" }
+    );
+    if (errors) console.error("Delete error:", errors);
+  }
 
   return (
     <main>
-     <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <h1>{user?.signInDetails?.loginId}’s todos</h1>
+      <button onClick={createTodo} disabled={!user}>
+        + new
+      </button>
       <ul>
         {todos.map((todo) => (
-     
-              
-          <li
-          onClick={() => deleteTodo(todo.id)}
-            
-            key={todo.id}>{todo.content}</li>
+          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
+            {todo.content}
+          </li>
         ))}
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+        <a
+          href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Review next step of this tutorial.
         </a>
       </div>
-
-            <button onClick={signOut}>Sign out</button>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
-
 
 export default App;
 
